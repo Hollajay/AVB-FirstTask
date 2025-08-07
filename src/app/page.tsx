de7c0c2c8d46d6bf1b0367bee312c87c86b2a29b
Hollajay <127396029+Hollajay@users.noreply.github.com>
2025-08-07 13:29:20 +0100

    first tak commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,109 @@
+"use client";
+import { useState } from "react";
+import { PlusIcon } from "@heroicons/react/16/solid";
+import { PencilIcon, TrashIcon } from "@heroicons/react/20/solid";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import { addTodo, removeTodo, editTodo } from "@/features/todoSlice";
 import Image from "next/image";
-
+import backgroundImage from "@/assets/images/appointment-calendar-2295338_1280.jpg";
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+  const todoList = useSelector((state: RootState) => state.todo.todos);
+  const dispatch = useDispatch();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [task, setTask] = useState("");
+
+  const submitTodo = (e: any) => {
+    e.preventDefault();
+    if (editIndex !== null) {
+      dispatch(editTodo({ index: editIndex, newTask: task }));
+      setEditIndex(null);
+    } else {
+      dispatch(addTodo({ task }));
+    }
+    setTask("");
+  };
+
+  const handleEdit = (index: number) => {
+    const currentTask = todoList[index].task;
+    setTask(currentTask);
+    setEditIndex(index);
+  };
+
+  const deleteTodo = (i: number) => {
+    dispatch(removeTodo(i));
+  };
+
+  const [checked, setChecked] = useState<number | null>(null);
+
+  return (
+    <div className="relative">
+     <div className="w-full h-screen"><Image className="w-full h-full" src={backgroundImage} alt="background image" /></div> 
+      <div className="w-full h-screen absolute left-0 top-0 bg-[#00000090] flex items-center justify-center">
+        <div className="md:w-1/2 w-full mx-4 bg-[#ffffff35] p-4 rounded-3xl  relative">
+          <div>
+            <h1 className="text-4xl">To Do</h1>
+          </div>
+
+          <form action="" onSubmit={submitTodo}>
+            <div className="p-4 space-y-6 flex items-center justify-between gap-6">
+              <div className="border-b-2 h-10 w-full ">
+                <input
+                  className="w-full h-full p-0 outline-0"
+                  placeholder="Enter todo"
+                  value={task}
+                  onChange={(e) => setTask(e.target.value)}
+                  type="text"
+                />
+              </div>
+              <button
+                className="bg-blue-300 px-6 py-2 rounded-lg"
+                type="submit"
+              >
+                <PlusIcon width={20} />
+              </button>
+            </div>
+          </form>
+
+          <div className="w-full flex flex-col gap-6 p-2">
+            {todoList.map((data, index) => (
+              <div
+                key={index}
+                className="shadow-2xl bg-[#ffffff97] p-3 rounded-lg items-center w-full justify-between flex hover:bg-blue-300"
+              >
+                <div className="flex gap-3">
+                  <input
+                    className="w-4"
+                    type="checkbox"
+                    checked={checked === index}
+                    onChange={() =>
+                      setChecked(checked === index ? null : index)
+                    }
+                  />
+                  <h1
+                    className={`font-normal ${
+                      checked === index
+                        ? "line-through italic text-neutral-700"
+                        : ""
+                    }`}
+                  >
+                    {data.task}
+                  </h1>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div onClick={() => handleEdit(index)}>
+                    <PencilIcon width={20} color="blue"/>
+                  </div>
+                  <div className="" onClick={() => deleteTodo(index)}>
+                    <TrashIcon width={20} color="#9e0505" />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
---
+++ src/features/todoSlice.ts
@@ -0,0 +1,27 @@
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState: {
+    todos: [
+      { task: "I want to go and watch" },
+      { task: "how are you doing today" },
+    ],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    editTodo: (state, action) => {
+      const { index, newTask } = action.payload;
+      state.todos[index].task = newTask;
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((_, i) => i !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions;
+export default todoSlice.reducer;
